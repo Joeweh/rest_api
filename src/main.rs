c7077dd4a178769614c62aa1c4d40ee7da9dc942
Joseph Siracusa <joeyqsa@outlook.com>
2022-10-19 04:27:38 -0400

    Added Function To Test Reading From MongoDB

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::str::FromStr;
+
 use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use mongodb::{bson::{doc, Document, oid::ObjectId}, options::ClientOptions, Client};
 
 #[get("/")]
 async fn root() -> impl Responder {
-    HttpResponse::Ok().body("API Online!")
+    let client_options = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await;
+    
+    let client = Client::with_options(client_options.unwrap()).unwrap();
+
+    let database = client.database("testDB");
+
+    let collection = database.collection::<Document>("users");
+
+    let user = collection.find_one(doc! { "_id": ObjectId::from_str("634f9e184b67156408039b7e").unwrap() }, None).await.unwrap().unwrap_or_else(|| doc! { "username": "error"});
+
+    HttpResponse::Ok().json(user)
 }
 
 #[actix_web::main] // or #[tokio::main]
