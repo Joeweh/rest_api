4ad14bfd2c018f8316e48b71e86c1e2c13c6efba
Joseph Siracusa <joeyqsa@outlook.com>
2022-10-21 02:49:24 -0400

    Added the following: - Futures package to help resolve db results - Route to get all users - Changed get user by id route to be dynamic based upon path variable - Added explicit types to all variables

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use std::str::FromStr;
 
+use futures::stream::{TryStreamExt};
+
 use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
-use mongodb::{bson::{doc, Document, oid::ObjectId}, options::ClientOptions, Client};
+use mongodb::{bson::{doc, Document, oid::ObjectId}, Cursor, Collection, options::ClientOptions, Client, Database};
 
-#[get("/")]
-async fn root() -> impl Responder {
-    let client_options = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await;
+#[get("/api/users")]
+async fn get_users() -> impl Responder {
+    let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
     
-    let client = Client::with_options(client_options.unwrap()).unwrap();
+    let client: Client = Client::with_options(client_options).unwrap();
 
-    let database = client.database("testDB");
+    let database: Database = client.database("testDB");
 
-    let collection = database.collection::<Document>("users");
+    let collection: Collection<Document> = database.collection::<Document>("users");
 
-    let user = collection.find_one(doc! { "_id": ObjectId::from_str("634f9e184b67156408039b7e").unwrap() }, None).await.unwrap().unwrap_or_else(|| doc! { "username": "error"});
+    let cursor: Cursor<Document> = collection.find(None, None).await.unwrap();
+
+    let list: Vec<Document> = cursor.try_collect().await.unwrap();
+
+    HttpResponse::Ok().json(list)
+}
+
+#[get("/api/users/{uid}")]
+async fn get_user_by_uid(uid: web::Path<String>) -> impl Responder {
+    let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
+    
+    let client: Client = Client::with_options(client_options).unwrap();
+
+    let database: Database = client.database("testDB");
+
+    let collection: Collection<Document> = database.collection::<Document>("users");
+
+    let user: Document = collection.find_one(doc! { "_id": ObjectId::from_str(&uid).unwrap() }, None).await.unwrap().unwrap();
 
     HttpResponse::Ok().json(user)
 }
@@ -22,7 +41,8 @@ async fn root() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(root)
+            .service(get_user_by_uid)
+            .service(get_users)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
