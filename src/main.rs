cb36a2825e8719598cb4fe29a36f3f13308f9730
Joseph Siracusa <joeyqsa@outlook.com>
2022-10-23 03:49:12 -0400

    Added the following: - Added actix cors to allow request access from all origins (less painful to test) - Added User struct for easier json serialization and deserialization of user data using serde - Added Post route for saving new users to mongodb database - Added mongodb client as shared app data to prevent multiple client instances from being created

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use std::str::FromStr;
 
-use futures::stream::{TryStreamExt};
+use futures::stream::TryStreamExt;
 
-use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
 use mongodb::{bson::{doc, Document, oid::ObjectId}, Cursor, Collection, options::ClientOptions, Client, Database};
+use serde::Deserialize;
+use serde::Serialize;
+use actix_cors::Cors;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    username: String
+}
 
 #[get("/api/users")]
-async fn get_users() -> impl Responder {
-    let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
-    
-    let client: Client = Client::with_options(client_options).unwrap();
-
-    let database: Database = client.database("testDB");
+async fn get_users(db_client: web::Data<Client>) -> impl Responder {
+    let database: Database = db_client.database("testDB");
 
     let collection: Collection<Document> = database.collection::<Document>("users");
 
@@ -23,12 +27,8 @@ async fn get_users() -> impl Responder {
 }
 
 #[get("/api/users/{uid}")]
-async fn get_user_by_uid(uid: web::Path<String>) -> impl Responder {
-    let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
-    
-    let client: Client = Client::with_options(client_options).unwrap();
-
-    let database: Database = client.database("testDB");
+async fn get_user_by_uid(db_client: web::Data<Client>, uid: web::Path<String>) -> impl Responder {
+    let database: Database = db_client.database("testDB");
 
     let collection: Collection<Document> = database.collection::<Document>("users");
 
@@ -37,10 +37,28 @@ async fn get_user_by_uid(uid: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().json(user)
 }
 
-#[actix_web::main] // or #[tokio::main]
+#[post("/api/users")]
+async fn save_user(db_client: web::Data<Client>, body: web::Json<User>) -> impl Responder {
+    let database: Database = db_client.database("testDB");
+
+    let collection: Collection<Document> = database.collection::<Document>("users");
+
+    collection.insert_one(doc! { "username": body.username.as_str() }, None).await.unwrap();
+
+    HttpResponse::Ok().json(body)
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.l4l1afu.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
+
+    let client = Client::with_options(client_options).unwrap();
+
+    HttpServer::new(move || {
+        Cors::permissive();
         App::new()
+            .app_data(web::Data::new(client.clone()))
+            .service(save_user)
             .service(get_user_by_uid)
             .service(get_users)
     })
