c3631430fe1be77c30c6dbef0f4b5e84640ad586
Joseph Siracusa <joeyqsa@outlook.com>
2022-10-26 00:02:39 -0400

    Added the following: - Created health check route to limit api downtime - Mapped port dynamically based upon whether server is executed in local or remote environment

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,30 @@ mod user;
 mod user_repo;
 mod user_controller;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, get, Responder, HttpResponse};
 use mongodb::{options::ClientOptions, Client};
 
 use actix_cors::Cors;
 use user_controller::{save_user, update_user, delete_user, get_user, get_users};
 use user_repo::UserRepository;
 
+use std::env;
+
+#[get("/api")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let client_options: ClientOptions = ClientOptions::parse("mongodb+srv://admin:admin@cluster0.xx20tsi.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
 
-    let client = Client::with_options(client_options).unwrap();
+    let client: Client = Client::with_options(client_options).unwrap();
+
+    let port: u16 = match env::var("PORT") {
+        Ok(port_string) => port_string.parse::<u16>().unwrap(),
+        Err(_error) => 8080
+    };
 
     HttpServer::new(move || {
         Cors::permissive();
@@ -25,8 +37,10 @@ async fn main() -> std::io::Result<()> {
             .service(delete_user)
             .service(get_user)
             .service(get_users)
+
+            .service(health_check)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
